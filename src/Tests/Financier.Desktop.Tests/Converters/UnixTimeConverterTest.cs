e3f421ce9debbbad76ca21657d0aff321b691dd4
vov4uk <khmel@outlook.com>
2022-01-14 12:06:02 +0200

    Fix test

--- src/Tests/Financier.Desktop.Tests/Converters/UnixTimeConverterTest.cs
+++ src/Tests/Financier.Desktop.Tests/Converters/UnixTimeConverterTest.cs
@@ -11,15 +11,19 @@
     {
         private readonly IValueConverter converter = new UnixTimeConverter();
 
-        [InlineAutoData(1642152563000, "2022-01-14 11:29:23")]
-        [InlineAutoData(1642111200000, "2022-01-14")]
+        [InlineAutoData(1642152563000, "2022-01-14 13:29:23")]
+        [InlineAutoData(1642111200000, "2022-01-14 02:00:00")]
         [Theory]
         public void Convert_GotParameters_ExpectedValues(object value, string expexted)
         {
+            string exp;
+            DateTime date = DateTime.Parse(expexted).ToUniversalTime();
+            exp = date == date.Date ? date.ToString(UnixTimeConverter.FORMAT_DAY) : date.ToString(UnixTimeConverter.FORMAT);
+
             // Act
             var actual = this.converter.Convert(value, null, null, CultureInfo.InvariantCulture);
 
-            Assert.Equal(expexted, actual);
+            Assert.Equal(exp, actual);
         }
 
         [InlineAutoData("2022-01-14 11:29:23", 1642152563000)]
@@ -27,7 +31,7 @@
         [Theory]
         public void ConvertBack_GotParameters_ExpectedValues(string value, long expexted)
         {
-            DateTime dateTime = DateTime.Parse(value);
+            DateTime dateTime = DateTime.Parse(value).ToUniversalTime();
 
             // Act
             var actual = this.converter.ConvertBack(dateTime, null, null, CultureInfo.InvariantCulture);
