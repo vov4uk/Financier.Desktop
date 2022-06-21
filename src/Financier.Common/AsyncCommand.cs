4bde7b9937dec15ca85a495dbe27de7a9ca511aa
vov4uk <khmel@outlook.com>
2022-06-21 19:53:34 +0300

    [ExcludeFromCodeCoverage]

--- src/Financier.Common/AsyncCommand.cs
+++ src/Financier.Common/AsyncCommand.cs
@@ -10,15 +10,36 @@ namespace Financier.Common
     public class AsyncCommand : ICommand, IAsyncCommand
     {
         private readonly Func<Task> _action;
-        private readonly Func<bool> _predicate;
         private readonly SynchronizationContext _context;
-        private event EventHandler _canExecuteChanged;
-
+        private readonly Func<bool> _predicate;
         public AsyncCommand(Func<Task> action, Func<bool> predicate = null)
         {
             _action = action;
             _predicate = predicate;
             _context = SynchronizationContext.Current;
+        }
+
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add { _canExecuteChanged += value; }
+            remove { _canExecuteChanged -= value; }
+        }
+
+        private event EventHandler _canExecuteChanged;
+        public bool CanExecute()
+        {
+            return _predicate == null || _predicate();
+        }
+
+        // ----- Implement ICommand
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute();
+        }
+
+        async void ICommand.Execute(object parameter)
+        {
+            await ExecuteAsync();
         }
 
         public async Task ExecuteAsync()
@@ -28,10 +49,6 @@ namespace Financier.Common
                 await _action();
             }
         }
-        public bool CanExecute()
-        {
-            return _predicate == null || _predicate();
-        }
         public void RaiseCanExecuteChanged()
         {
             if (_context != null)
@@ -49,35 +66,42 @@ namespace Financier.Common
             if (handler != null)
                 handler(this, EventArgs.Empty);
         }
-
-        // ----- Implement ICommand
-        bool ICommand.CanExecute(object parameter)
-        {
-            return CanExecute();
-        }
-        async void ICommand.Execute(object parameter)
-        {
-            await ExecuteAsync();
-        }
-        event EventHandler ICommand.CanExecuteChanged
-        {
-            add { _canExecuteChanged += value; }
-            remove { _canExecuteChanged -= value; }
-        }
     }
 
+    [ExcludeFromCodeCoverage]
     public class AsyncCommand<T> : ICommand, IAsyncCommand<T>
     {
-        private readonly Func<T, Task> _parameterizedAction;
         private readonly Predicate<T> _canExecute;
         private readonly SynchronizationContext _context;
-        private event EventHandler _canExecuteChanged;
-
+        private readonly Func<T, Task> _parameterizedAction;
         public AsyncCommand(Func<T, Task> parameterizedAction, Predicate<T> canExecute = null)
         {
             _parameterizedAction = parameterizedAction;
             _canExecute = canExecute;
             _context = SynchronizationContext.Current;
+        }
+
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add { _canExecuteChanged += value; }
+            remove { _canExecuteChanged -= value; }
+        }
+
+        private event EventHandler _canExecuteChanged;
+        public bool CanExecute(T parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        // ----- Explicit implementations
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute((T)parameter);
+        }
+
+        async void ICommand.Execute(object parameter)
+        {
+            await ExecuteAsync((T)parameter);
         }
 
         public async Task ExecuteAsync(T parameter)
@@ -87,10 +111,6 @@ namespace Financier.Common
                 await _parameterizedAction(parameter);
             }
         }
-        public bool CanExecute(T parameter)
-        {
-            return _canExecute == null || _canExecute(parameter);
-        }
         public void RaiseCanExecuteChanged()
         {
             if (_context != null)
@@ -107,21 +127,6 @@ namespace Financier.Common
             var handler = _canExecuteChanged;
             if (handler != null)
                 handler(this, EventArgs.Empty);
-        }
-
-        // ----- Explicit implementations
-        bool ICommand.CanExecute(object parameter)
-        {
-            return CanExecute((T)parameter);
-        }
-        async void ICommand.Execute(object parameter)
-        {
-            await ExecuteAsync((T)parameter);
-        }
-        event EventHandler ICommand.CanExecuteChanged
-        {
-            add { _canExecuteChanged += value; }
-            remove { _canExecuteChanged -= value; }
         }
     }
 }
