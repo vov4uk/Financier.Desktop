068ae534e3e8dff9a6350622063e344abeacac8f
vov4uk <khmelovskyi@outlook.com>
2026-06-17 09:28:33 +0200

    remove CellTemplateAttribute

--- src/Financier.Common/Controls/DataGridAutoHeaders.cs
+++ src/Financier.Common/Controls/DataGridAutoHeaders.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 using System.Windows.Controls;
-using Financier.Common.Attribute;
 using Financier.Common.Localization;
 
 namespace Financier.Common.Controls
@@ -13,7 +12,7 @@ namespace Financier.Common.Controls
         public DataGridAutoHeaders()
         {
             AutoGenerateColumns = true;
-            AutoGeneratingColumn += DataGridAutoHeadersAutoGeneratingColumn!;
+            AutoGeneratingColumn += DataGridAutoHeadersAutoGeneratingColumn;
         }
 
         private void DataGridAutoHeadersAutoGeneratingColumn(
@@ -21,62 +20,32 @@ namespace Financier.Common.Controls
           DataGridAutoGeneratingColumnEventArgs e)
         {
             string propertyDisplayName = GetPropertyDisplayName(e.PropertyDescriptor);
-            string propertyStyle = GetPropertyCellTemplateName(e.PropertyDescriptor);
             string name = e.PropertyType.Name;
 
-            if (!string.IsNullOrEmpty(propertyStyle))
+            if (!string.IsNullOrEmpty(propertyDisplayName))
             {
-                var column = new DataGridTemplateColumn();
-                column.CellTemplate = FindResource(propertyStyle) as DataTemplate;
-                if (!string.IsNullOrEmpty(propertyDisplayName))
-                {
-                    column.Header = propertyDisplayName;
-                }
-                else
-                {
-                    column.Visibility = Visibility.Collapsed;
-                }
-
-                e.Column = column;
+                e.Column.Header = LocalizationService.Instance[propertyDisplayName];
             }
             else
             {
-                if (!string.IsNullOrEmpty(propertyDisplayName))
-                {
-                    e.Column.Header = LocalizationService.Instance[propertyDisplayName];
-                }
-                else
-                {
-                    e.Column.Visibility = Visibility.Collapsed;
-                }
-
-                if (name != typeof(long).Name && name != typeof(long?).Name && name != typeof(double).Name && name != typeof(double?).Name)
-                    return;
-                e.Column.CellStyle = new Style(typeof(DataGridCell));
-                e.Column.CellStyle.Setters.Add(new Setter(HorizontalAlignmentProperty, HorizontalAlignment.Right));
+                e.Column.Visibility = Visibility.Collapsed;
             }
+
+            if (name != typeof(long).Name && name != typeof(long?).Name && name != typeof(double).Name && name != typeof(double?).Name)
+                return;
+            e.Column.CellStyle = new Style(typeof(DataGridCell));
+            e.Column.CellStyle.Setters.Add(new Setter(HorizontalAlignmentProperty, HorizontalAlignment.Right));
         }
 
         private static string GetPropertyDisplayName(object p)
         {
-            PropertyDescriptor propertyDescriptor = (p as PropertyDescriptor)!;
+            PropertyDescriptor propertyDescriptor = (p as PropertyDescriptor);
             string propertyDisplayName = null;
             if (propertyDescriptor != null && propertyDescriptor.Attributes[typeof(DisplayNameAttribute)] is DisplayNameAttribute attribute && attribute != DisplayNameAttribute.Default)
             {
                 propertyDisplayName = attribute.DisplayName;
             }
             return propertyDisplayName;
-        }
-
-        private static string GetPropertyCellTemplateName(object p)
-        {
-            PropertyDescriptor propertyDescriptor = (p as PropertyDescriptor)!;
-            string proprtyDisplayName = null;
-            if (propertyDescriptor != null && propertyDescriptor.Attributes[typeof(CellTemplateAttribute)] is CellTemplateAttribute attribute)
-            {
-                proprtyDisplayName = attribute.Key;
-            }
-            return proprtyDisplayName;
         }
     }
 }
